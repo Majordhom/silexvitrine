6f223c097addab83befa19a96eb6dc1f1fdb8b59
ericbianchi <ulee13@gmail.com>
2025-06-02 16:15:37 +0200

    Silex Vitrine (Navbar + Pages + divers test Fonts)

---
+++ src/app/_lib/fonts.ts
@@ -0,0 +1,57 @@
+import localFont from 'next/font/local'
+
+export const roboto = localFont({
+    variable: '--font-roboto',
+    display: 'swap',
+    src: [
+        {
+            path: '../public/fonts/Roboto-Black.ttf',
+            weight: '900',
+            style:'normal'
+        },{
+            path: '../public/fonts/Roboto-Bold.ttf',
+            weight: '700',
+            style:'normal'
+        },{
+            path: '../public/fonts/Roboto-Regular.ttf',
+            weight: '600',
+            style:'normal'
+        }, {
+            path: '../public/fonts/Roboto-Medium.ttf',
+            weight: '400',
+            style:'normal'
+        }, {
+            path: '../public/fonts/Roboto-Light.ttf',
+            weight: '300',
+            style:'normal'
+        }
+    ]
+});
+
+export const titillium = localFont({
+    variable: '--font-titillium',
+    display: 'swap',
+    src: [
+        {
+            path: '../public/fonts/TitilliumWeb-Black.ttf',
+            weight: '900',
+            style:'normal'
+        },{
+            path: '../public/fonts/TitilliumWeb-Bold.ttf',
+            weight: '700',
+            style:'normal'
+        },{
+            path: '../public/fonts/TitilliumWeb-SemiBold.ttf',
+            weight: '600',
+            style:'normal'
+        }, {
+            path: '../public/fonts/TitilliumWeb-Regular.ttf',
+            weight: '400',
+            style:'normal'
+        }, {
+            path: '../public/fonts/TitilliumWeb-Light.ttf',
+            weight: '300',
+            style:'normal'
+        }
+    ]
+});
---
+++ src/app/_lib/ui-kit/components/navbar.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import { usePathname } from "next/navigation";
+import { Url } from "next/dist/shared/lib/router/router";
+
+interface INavbarProps {}
+
+const navItems = [
+    {
+        id: "accueil",
+        label: "Accueil",
+        href: "/",
+    },
+    {
+        id: "annonces",
+        label: "Annonces",
+        href: "/annonces",
+    },
+    {
+        id: "contact",
+        label: "Contact",
+        href: "/contact",
+    },
+];
+
+const Navbar: React.FunctionComponent<INavbarProps> = (props) => {
+    const pathname = usePathname();
+    const isActive = (path: Url) => pathname === path;
+
+    return (
+        <nav className="pb-4 md:pb-8 flex justify-between items-center">
+            <Link
+                href="/"
+                className="text-lg md:text-3xl font-bold text-spotify-green"
+            >
+                webapp
+            </Link>
+            <ul className="flex justify-end items-center gap-4">
+                {navItems.map((eachItem) => (
+                    <li key={eachItem.id}>
+                        <Link
+                            href={eachItem.href}
+                            className={`${
+                                isActive(eachItem.href) ? "text-spotify-green" : ""
+                            }`}
+                        >
+                            {eachItem.label}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
+
+
+
