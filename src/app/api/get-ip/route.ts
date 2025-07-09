4bc4edfae8fcc6b20c8023de64e4799a835a1b30
Mohamed Zakaria <ahmedzaki4701@hotmail.com>
2025-07-09 11:49:48 +0200

    Add `get-ip` API route to fetch client IP address
    - Introduce a `GET` handler to return the client's IP using the `x-forwarded-for` header or a default value.

---
+++ src/app/api/get-ip/route.ts
@@ -0,0 +1,8 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function GET(req: NextRequest) {
+    const ip = req.headers.get("x-forwarded-for") || "inconnue";
+    return NextResponse.json({ ip });
+}
+
+//cette route est utilisée pour obtenir l'adresse IP du client
